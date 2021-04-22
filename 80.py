1152464a9a392b9e4f65b5d849e7f03564d06af7
Daashaa <72604325+Daashaa@users.noreply.github.com>
2021-04-23 02:24:10 +0500

    Update 80.py

--- 80.py
+++ 80.py
@@ -14,57 +14,29 @@
 Создать прямоугольную матрицу A, имеющую N строк и M столбцов со случайными элементами.
 Разделить элементы матрицы на элемент матрицы с наибольшим значением.
 """
-import random
+import numpy as np 
 N = int(input("Введите количество строк: "))
 M = int(input("Введите количество столбцов: "))
-def get_row(column):
-    col = []
-    for i in range(0, column):
-        col.append(random.randint(0, 99))
-    return col
-def get_matrix(row, column):
-    matrix = []
-    for i in range(0, row):
-        matrix.append(get_row(column))
-    return matrix
-def print_matrix(matrix):
-    i = 0
-    while i < len(matrix):
-        j = 0
-        row = matrix[i]
-        while j < len(row):
-            column = row[j]
-            print(column, end=' ')
-            j += 1
-        print()
-        i += 1
-def get_column(matrix, index):
-    column = []
-    i = 0
-    while i < len(matrix):
-        column.append(matrix[i][index])
-        i += 1
-    return column
-A = get_matrix(N, M)
-print("Исходная матрица:")
-print_matrix(A)
-max_element = False
+print(f'\nЗадан размер матрицы случайных целых чисел: [{N}, {M}]')
+np.random.seed(0)
+A = np.random.randint(10, size = (N, M))
+print('\nПрямоугольная матрица случайных целых чисел:\n', A)
+max_z = False
 i = 0
 while i < len(A):
     j = 0
     while j < len(A[i]):
-        if max_element is False or max_element < A[i][j]:
-            max_element = A[i][j]
+        if max_z is False or max_z < A[i][j]:
+            max_z = A[i][j]
         j += 1
     i += 1
 i = 0
 while i < len(A):
     j = 0
     while j < len(A[i]):
-        A[i][j] /= max_element
+        A[i][j] /= max_z
         A[i][j] = round(A[i][j], 1)
         j += 1
     i += 1
-print("Максимальный элемент:", max_element)
-print("Модифицированная матрица:")
-print_matrix(A)
+print("Максимальный элемент:", max_z)
+print("\nМодифицированная матрица:\n", A)
